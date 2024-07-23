acc830268f74208230d4c100d2de0e5ec87ab977
luboslenco <lubos.lenco@gmail.com>
2024-07-24 00:00:08 +0200

    Use project.js

--- project.js
+++ project.js
@@ -2,13 +2,8 @@ let project = new Project('krafix');
 
 project.addDefine('SPIRV_CROSS_KRAFIX');
 project.addDefine('KRAFIX_LIBRARY');
-project.cpp11 = true;
 
-project.addExclude('.git/**');
-project.addExclude('glslang/.git/**');
-project.addExclude('build/**');
 project.addFile('Sources/**');
-
 project.addFile('glslang/StandAlone/ResourceLimits.cpp');
 project.addFile('glslang/glslang/GenericCodeGen/**');
 project.addFile('glslang/glslang/MachineIndependent/**');
@@ -22,7 +17,7 @@ project.addIncludeDir('glslang/glslang/MachineIndependent');
 project.addIncludeDir('glslang/glslang/Include');
 project.addIncludeDir('glslang/OGLCompilersDLL');
 
-if (platform === Platform.Windows) {
+if (platform === "windows") {
 	project.addFile('glslang/glslang/OSDependent/Windows/**');
 	project.addIncludeDir('glslang/glslang/OSDependent/Windows');
 }
@@ -31,4 +26,4 @@ else {
 	project.addIncludeDir('glslang/glslang/OSDependent/Unix');
 }
 
-resolve(project);
+return project;
